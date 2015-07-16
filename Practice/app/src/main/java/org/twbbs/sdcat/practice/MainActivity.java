d38732dce961d22516508d1571c0af92cef15c7c
SDCAT <sdcatbbs@gmail.com>
2015-07-16 20:59:06 +0800

    Implement click method and hide checkbox

--- Practice/app/src/main/java/org/twbbs/sdcat/practice/MainActivity.java
+++ Practice/app/src/main/java/org/twbbs/sdcat/practice/MainActivity.java
@@ -2,16 +2,41 @@ package org.twbbs.sdcat.practice;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+//import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private EditText editText;
+    private CheckBox hideCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //先繪出畫面後(setContentView)，才能從目前的畫面中取得物件實体。
+        editText = (EditText) findViewById(R.id.editText);
+        //editText.setText("Enter New Text Here.");
+        hideCheckBox= (CheckBox) findViewById(R.id.checkBox);
+    }
+
+    //onClick需為public , Arg需有View
+    public void submit(View view){
+        String text= editText.getText().toString();
+        //Log.d("debug", "edittext: " + text);
+        //duration : 一個常數, 設定不同的顯示時間 0 SHORT, 1 LONG
+
+        if(hideCheckBox.isChecked()) {
+            text = "********";
+        }
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+        editText.setText("");
     }
 
     @Override
